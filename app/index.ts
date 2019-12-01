9417c56a94e7f8cc53fc91c740ee9259a5e674cd
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-12-01 23:18:17 +0900

    LoadBalancerとFargate構築の完成

---
+++ app/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello World");
+});
+
+app.listen(3000);
---
+++ cdk/lib/stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from "@aws-cdk/core";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as elb from "@aws-cdk/aws-elasticloadbalancingv2";
+
+export class MyStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const cluster = new ecs.Cluster(this, "SampleCluster", {
+      clusterName: "SmapleCluster"
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, "TaskDef");
+
+    const container = taskDefinition.addContainer("DefaultContainer", {
+      image: ecs.ContainerImage.fromAsset("../app"),
+      memoryLimitMiB: 512,
+      cpu: 256
+    });
+
+    container.addPortMappings({
+      containerPort: 3000
+    });
+
+    const ecsService = new ecs.FargateService(this, "Service", {
+      cluster,
+      taskDefinition,
+      desiredCount: 2
+    });
+
+    const lb = new elb.ApplicationLoadBalancer(this, "LB", {
+      vpc: cluster.vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener("Listener", { port: 80 });
+
+    const targetGroup = listener.addTargets("ECS", {
+      protocol: elb.ApplicationProtocol.HTTP,
+      port: 3000,
+      targets: [ecsService]
+    });
+  }
+}
